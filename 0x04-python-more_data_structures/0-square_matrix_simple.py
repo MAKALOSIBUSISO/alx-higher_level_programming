72ecf09921f651ceec8e4f3ee7620ffb49fcace4
MAKALOSIBUSISO <sbusisomakalo@gmail.com>
2023-06-14 04:25:39 -0700

    Square simple

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,6 +1,15 @@
 #!/usr/bin/python3
-def square_matrix_simple(matrix=[]):
-    squared = []
-    for line in matrix:
-        squared.append([c**2 for in line])
-        return squared
+
+def uniq_add(my_list=[]):
+
+    uniq_list = set(my_list)
+
+    num = 0
+
+
+    for i in uniq_list:
+
+        num += i
+
+
+    return (num)
